0e93ec01f8d6b39bedd215f71d50c6fb49faac2d
Tanya Phung <10180091+tanyaphung@users.noreply.github.com>
2020-05-12 08:47:43 -0400

    Add files via upload

--- calc_num_site_in_vcf.py
+++ calc_num_site_in_vcf.py
@@ -1,32 +1,42 @@
 import argparse
 import gzip
 
+
 def parse_args():
-	"""
-	Parse command-line arguments
-	"""
-	parser = argparse.ArgumentParser(description="Calculate the number of sites in a vcf file")
+    """
+    Parse command-line arguments
+    """
+    parser = argparse.ArgumentParser(description="Calculate the number of sites in a vcf file")
 
-	parser.add_argument(
-            "--vcf", required=True,
-            help="REQUIRED. Input the path to the vcf file.")
+    parser.add_argument(
+        "--vcf", required=True,
+        help="REQUIRED. Input the path to the vcf file.")
 
-	args = parser.parse_args()
-	return args
+    parser.add_argument(
+        "--id", required=True,
+        help="REQUIRED. Input the id of the file")
+
+    args = parser.parse_args()
+    return args
+
 
 def file_test(vcf_file):
-	"""
+    """
     This function checks if the input VCF file is gzip or not.
     """
-	if vcf_file.endswith('.gz'):
-		return gzip.open, 'rb'
-	else:
-		return open, 'rb'
+    if vcf_file.endswith('.gz'):
+        return gzip.open, 'rb'
+    else:
+        return open, 'rb'
+
 
 def main():
-	args = parse_args()
+    args = parse_args()
 
-	open_func, mode = file_test(args.vcf)
-	with open_func(args.vcf, mode) as f:
-		print (sum([1 for line in f if not line.startswith(b'#')]))
+    open_func, mode = file_test(args.vcf)
+    with open_func(args.vcf, mode) as f:
+        out = [args.id, str(sum([1 for line in f if not line.startswith(b'#')]))]
+        print (','.join(out))
+
+
 main()
