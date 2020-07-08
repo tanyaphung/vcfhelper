6c86180fef1346d7577d1f03ddaa839fda53bd78
Tanya Phung <tanya.n.phung@gmail.com>
2020-07-08 09:54:15 -0400

    fix byte

--- convert_vcf_to_bed.py
+++ convert_vcf_to_bed.py
@@ -14,21 +14,21 @@ def file_test(vcf_file):
     This function checks if the input VCF file is gzip or not.
     """
     if vcf_file.endswith(".gz"):
-        return gzip.open, "rb"
+        return gzip.open, "rt"
     else:
-        return open, "rb"
+        return open, "rt"
 
 def main():
     args = parse_args()
-    outfile = open(args.output_bed, "wb")
+    outfile = open(args.output_bed, "wt")
     open_func, mode = file_test(vcf_file=args.input_vcf)
 
     with open_func(args.input_vcf, mode) as f:
         for line in f:
-            if not line.startswith(b"#"):
-                items = line.rstrip(b"\n").split(b"\t")
+            if not line.startswith("#"):
+                items = line.rstrip("\n").split("\t")
                 out = [items[0], str(int(items[1]) - 1), items[1]]
-                print(b"\t".join(out), file=outfile)
+                print("\t".join(out), file=outfile)
 
 if __name__ == '__main__':
     main()
