71547c8c4a6a142e8f1e757d995a3fea92bf424c
Tanya Phung <tanya.n.phung@gmail.com>
2020-07-08 09:37:19 -0400

    add options for gzip vcf file

--- convert_vcf_to_bed.py
+++ convert_vcf_to_bed.py
@@ -1,16 +1,34 @@
 import argparse
+import gzip
 
-parser = argparse.ArgumentParser(description="Convert vcf to bed file format.")
-parser.add_argument("--input_vcf",required=True,help="Input the path to the vcf file")
-parser.add_argument("--output_bed",required=True,help="Input the path to the output file")
+def parse_args():
+    parser = argparse.ArgumentParser(description="Convert vcf to bed file format.")
+    parser.add_argument("--input_vcf",required=True,help="Input the path to the vcf file")
+    parser.add_argument("--output_bed",required=True,help="Input the path to the output file")
 
-args = parser.parse_args()
+    args = parser.parse_args()
+    return args
 
-outfile = open(args.output_bed, "w")
+def file_test(vcf_file):
+    """
+    This function checks if the input VCF file is gzip or not.
+    """
+    if vcf_file.endswith(".gz"):
+        return gzip.open, "r"
+    else:
+        return open, "r"
 
-with open(args.input_vcf, "r") as f:
-    for line in f:
-        if not line.startswith("#"):
-            items = line.rstrip("\n").split("\t")
-            out = [items[0], str(int(items[1])-1), items[1]]
-            print("\t".join(out), file=outfile)
+def main():
+    args = parse_args()
+    outfile = open(args.output_bed, "w")
+    open_func, mode = file_test(vcf_file=args.input_vcf)
+
+    with open_func(args.input_vcf, mode) as f:
+        for line in f:
+            if not line.startswith("#"):
+                items = line.rstrip("\n").split("\t")
+                out = [items[0], str(int(items[1]) - 1), items[1]]
+                print("\t".join(out), file=outfile)
+
+if __name__ == '__main__':
+    main()
